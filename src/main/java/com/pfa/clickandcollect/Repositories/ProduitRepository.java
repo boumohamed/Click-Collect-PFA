db42c47a96bb89926b49ae91c4fe8ac25738a860
Hicham Oubari <75031773+hichamob@users.noreply.github.com>
2022-04-08 00:59:36 +0000

    function delete

--- src/main/java/com/pfa/clickandcollect/Repositories/ProduitRepository.java
+++ src/main/java/com/pfa/clickandcollect/Repositories/ProduitRepository.java
@@ -1,7 +1,10 @@
 package com.pfa.clickandcollect.Repositories;
 
 import com.pfa.clickandcollect.Entities.Produit;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProduitRepository extends JpaRepository<Produit, Long> {
+    Page<Produit> findByNomPrdContains(String kw, Pageable pageable);
 }
